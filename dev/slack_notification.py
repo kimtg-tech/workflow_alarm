561688d46cafb9243012f121b74f524e35b0b964
tgkim <ktgstar@gmail.com>
2024-12-17 00:35:37 +0900

    dags error repair4

--- dev/slack_notification.py
+++ dev/slack_notification.py
@@ -1,19 +1,27 @@
-# shared/slack_notification.py
 import requests
+import sys 
 import json
 
-
 def send_slack_message(webhook_url, message):
-    """
-    Send a message to Slack using a webhook URL.
+    title = ("New Incoming Message :zap:")
+    slack_data = {
+        "username" : "notice_boot",    
+        "attachments" : [
+                {
+                    "color" : "#9733EE",
+                    "fields" : [
+                        {
+                        "title" : title, 
+                        "value" : message, 
+                        "short" : "false",
+                    }
+                ]
+            }        
+        ]
+    }
 
-    :param webhook_url: Slack Incoming Webhook URL
-    :param message: Message content to send
-    """
-    try:
-        headers = {'Content-Type': 'application/json'}
-        payload = {'text': message}
-        response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error sending message to Slack: {e}")
+    byte_length = str(sys.getsizeof(slack_data))
+    headers = {'Content-Type': "appication.json", 'Content-Length' : byte_length}
+    response = requests.post(webhook_url, data = json.dumps(slack_data), headers = headers)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
